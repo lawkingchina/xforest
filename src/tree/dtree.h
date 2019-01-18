371bef7957cb8b0951bb01f82a92381b33dcd3c0
aksnzhy <mctt90@gmail.com>
2019-01-18 22:07:45 +0800

    update

--- src/tree/dtree.h
+++ src/tree/dtree.h
@@ -28,6 +28,191 @@ This file defines the DTree class.
 
 namespace xforest {
 
+// Find maximal and minimal value for each feature
+struct MaxMin {
+  real_t gap = 0.0;
+  real_t max_feat = kFloatMin;
+  real_t min_feat = kFloatMax;
+};
+
+struct Histogram;
+struct DTNode;
+
+// Tmp information during training
+class TInfo {
+ public:
+  // ctor and dctor
+  TInfo(const index_t num_feat,
+        const index_t num_bin,
+        const uint8 num_class) {}
+  ~TInfo() {}
+  // left or right
+  char l_or_r;
+  // node layer
+  uint8 level = 1;
+  // start position
+  index_t start_pos = 0;
+  // end position
+  index_t end_pos = 0;
+  // mid position
+  index_t mid_pos = 0;
+  // best gini value
+  real_t best_gini = 1.0;
+  // parent node
+  DTNode* parent = nullptr;
+  // Brother node
+  DTNode* brother = nullptr;
+  // Histogram bin
+  Histogram* histo = nullptr;
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TInfo);
+};
+
+// Decision tree node
+class DTNode {
+ public:
+  // ctor and dctor
+  DTNode() {}
+  ~DTNode() {}
+  // If current node is a leaf node?
+  bool is_leaf = false;
+  // leaf node value
+  real_t leaf_val = -1.0;
+  // left child
+  DTNode* l_child = nullptr;
+  // right child
+  DTNode* r_child = nullptr;
+  // best feature id
+  index_t best_feat_id = 0;
+  // best bin value
+  uint8 best_bin_val = 0;
+  // Tmp info used by training
+  TInfo* info = nullptr;
+  // Initialize
+  inline void Init(const index_t num_feat,
+                   const index_t num_bin,
+                   const uint8 num_class) {
+    info = new TInfo(num_feat, num_bin, num_class);
+  }
+  // Clear TInfo
+  inline void Clear() { 
+    delete info;
+  }
+  // Clear parent info
+  inline void ClearParent() {
+    delete info->parent->info;
+  }
+  // Is a leaf node?
+  inline bool IsLeaf() {
+    return is_leaf;
+  }
+  inline void SetLeaf() {
+    is_leaf = true;
+  }
+  // value of leaf
+  inline real_t LeafVal() {
+    return leaf_val;
+  }
+  inline void SetLeafVal(real_t val) {
+    leaf_val = val;
+  }
+  // Left child
+  inline DTNode* LeftChild() {
+    return l_child;
+  }
+  inline void SetLeftChild() {
+    l_child = node;
+  }
+  // Right child
+  inline DTNode* RightChild() {
+    return r_child;
+  }
+  inline SetRightChild(DTNode* node) {
+    r_child = node;
+  }
+  // Best feature ID
+  inline index_t BestFeatID() {
+    return best_feat_id;
+  }
+  inline void SetBestFeatID(index_t id) {
+    best_feat_id = id;
+  }
+  // Best bin value
+  inline uint8 BestBinVal() {
+    return best_bin_val;
+  }
+  inline SetBestBinVal(uint8 val) {
+    best_bin_val = val;
+  }
+  // Left or Right
+  inline char LeftOrRight() {
+    return info->l_or_r;
+  }
+  inline void SetLeftOrRight(char ch) {
+    info->l_or_r = ch;
+  }
+  // Node level
+  inline uint8 Level() {
+    return info->level;
+  }
+  inline void SetLevel(uint8 level) {
+    info->level = level;
+  }
+  // Start position
+  inline index_t StartPos() {
+    return info->start_pos;
+  }
+  inline void SetStartPos(index_t pos) {
+    info->start_pos = pos;
+  }
+  // End position
+  inline index_t EndPos() {
+    return info->end_pos;
+  }
+  inline void SetEndPos(index_t pos) {
+    info->end_pos = pos;
+  }
+  // Mid position (split pos)
+  inline index_t MidPos() {
+    return info->mid_pos;
+  }
+  inline void SetMidPos(index_t pos) {
+    info->mid_pos = pos;
+  }
+  // Best gini
+  inline real_t BestGini() {
+    return info->best_gini;
+  }
+  inline void SetBestGini(real_t gini) {
+    info->best_gini = gini;
+  }
+  // Parent node
+  inline DTNode* Parent() {
+    return info->parent;
+  }
+  inline void SetParent(DTNode* node) {
+    info->parent = node;
+  }
+  // Brother node
+  inline DTNode* Brother() {
+    return info->brother;
+  }
+  inline void SetBrother(DTNode* node) {
+    info->brother = node;
+  }
+  // Histogram bin
+  inline Histogram* Histo() {
+    return info->histo;
+  }
+  inline void SetHisto(Histogram* hiso) {
+    info->histo = histo;
+  }
+  // Data size
+  inline index_t DataSize() {
+    return info->end_pos-info->start_pos+1;
+  }
+};
+
 //------------------------------------------------------------------------------
 // The DTree class is an abstract class, which will be implemented
 // by real decision tree, such as BTree (for binary classification), 
@@ -64,7 +249,7 @@ class DTree {
   }
 
  private:
-  uint8 max_depth_;  // Maximal depth to grow a tree
+  uint8 max_depth_;  // Maximal depth to grow a tree (< 256)
   index_t min_samples_split_;  // Minimal samples to split a node
   index_t min_samples_leaf_;  // Minimal samples in a leaf node
   index_t max_leaf_;  // Maximal number of leaf nodes
@@ -73,6 +258,8 @@ class DTree {
 
   std::vector<index_t> rowIdx_;  // Data sample
   std::vector<index_t> colIdx_;  // Feature sample
+
+  DTNode* root_ = nullptr; // root node
 
   DISALLOW_COPY_AND_ASSIGN(DTree);
 };
