2366602ad8a176a40a5ac1865a936ca92148edc5
aksnzhy <mctt90@gmail.com>
2019-01-18 19:56:07 +0800

    update

--- src/tree/dtree.h
+++ src/tree/dtree.h
@@ -25,129 +25,39 @@ This file defines the DTree class.
 
 #include <queue>
 #include <vector>
-#include <algorithm>
 
 namespace xforest {
 
-//--------------------------------------------------------
-// Find max and min value in every column data
-//--------------------------------------------------------
-struct MaxMin {
-  real_t gap = 0.0;
-  real_t max_feat = kFloatMin;
-  real_t min_feat = kFloatMax;
-};
-
-//--------------------------------------------------------
-// Histogram data structure
-//--------------------------------------------------------
-
-struct Histogram {
-  ~Histogram() {
-    for (size_t i = 0; i < bin_count.size(); ++i) {
-      if (bin_count[i] != nullptr) {
-        delete [] bin_count[i];
-      }
-    }
-  }
-  index_t count_len = 0;
-  index_t* count = nullptr;
-};
-
-//---------------------------------------------------
-// Tmp info during training
-//---------------------------------------------------
-struct DTNode;
-
-struct TInfo {
-  ~TInfo() {
-    delete histo;
-  }
-  // left or right
-  char l_or_r;
-  // node layer
-  uint32 level = 1;
-  // start postion
-  uint32 start_pos = 0;
-  // end position
-  uint32 end_pos = 0;
-  // split position
-  uint32 mid_pos = 0;
-  // Best gini value
-  real_t best_gini = 1.0;
-  // Parent node
-  DTNode* parent = nullptr;
-  // Brother node
-  DTNode* brother = nullptr;
-  // Histogram
-  Histogram* histo = nullptr;
-  // Init histogram
-  void InitHistoGram(const uint32 col_size, const uint8 max_bin) {
-    histo->bin_count.resize(col_size);
-    for (uint32_t i = 0; i < col_size; ++i) {
-      histo->bin_count[i] = new Count[max_bin+1];
-      for (uint32_t j = 0; j <= max_bin; ++j) {
-        histo->bin_count[i][j].count_0 = 0;
-        histo->bin_count[i][j].count_1 = 0;
-      }
-    }
-    histo->total_1 = 0;
-    histo->total_0 = 0;
-  }
-};
-
 //------------------------------------------------------------------------------
 // The DTree class is an abstract class, which will be implemented
-// by real decision tree, such as CTree (Classification Tree) and RTree 
-// (Regression Tree).
+// by real decision tree, such as BTree (for binary classification), 
+// MCTree (for multi-class classificaition), and RTree (for regression).
 //------------------------------------------------------------------------------
 class DTree {
  public:
   // ctor and dctor
   explicit DTree(const HyperParam& hyper_param) {
-    CHECK_GT(hyper_param.row_size, 0);
-    CHECK_GT(hyper_param.col_size, 0);
-    CHECK_GT(hyper_param.max_bin, 0);
-    CHECK_LT(hyper_param.max_bin, 256);
-    CHECK_GT(hyper_param.max_depth, 0);
-    CHECK_LT(hyper_param.max_depth, 256);
-    CHECK_GT(hyper_param.max_leaf, 0);
-    CHECK_GE(hyper_param.min_gini, 0);
-    CHECK_GT(hyper_param.min_split, 1);
-    CHECK_GT(hyper_param.col_sample, 0);
-    CHECK_LE(hyper_param.col_sample, 
-             hyper_param.col_size);
-    row_size_ = hyper_param.row_size;
-    col_size_ = hyper_param.col_size;
-    max_bin_ = hyper_param.max_bin;
+    CHECK_GT(hyper_param.max_depth, 1);
+    CHECK_LE(hyper_param.max_depth, 255);
+    CHECK_GE(hyper_param.min_samples_split, 2);
+    CHECK_GE(hyper_param.min_samples_leaf, 1);
+    CHECK_GE(hyper_param.max_leaf_nodes, 2);
     max_depth_ = hyper_param.max_depth;
-    max_leaf_ = hyper_param.max_leaf;
-    min_gini_ = hyper_param.min_gini;
-    min_split_ = hyper_param.min_split;
-    bootstrap_ = hyper_param.bootstrap;
-    col_sample_ = hyper_param.col_sample;
+    min_samples_split_ = hyper_param.min_samples_split;
+    min_samples_leaf_ = hyper_param.min_samples_leaf;
+    max_leaf_ = hyper_param.max_leaf_nodes;
+    min_impurity_dec_ = hyper_param.min_impurity_decrease;
+    min_impurity_ = hyper_param.min_impurity_split;
   }
   ~DTree() { }
 
  private:
-  // The number of data
-  uint32 row_size_;
-  // The number of feature
-  uint32 col_size_;
-  // The max index of histogram bin value
-  uint8 max_bin_;
-  // The max depth of tree
-  uint8 max_depth_;
-  // The max number of leaves in tree
-  uint16 max_leaf_;
-  // The minimal gini val in node to split
-  real_t min_gini_;
-  // The minimal examples in node to split
-  uint16 min_split_;
-  // If using bootstrap
-  bool bootstrap_ ;
-  // The number of feature to use
-  uint32 col_sample_;
+  uint8 max_depth_;  // Maximal depth to grow a tree
+  index_t min_samples_split_;  // Minimal samples to split a node
+  index_t min_samples_leaf_;  // Minimal samples in a leaf node
+  index_t max_leaf_;  // Maximal number of leaf nodes
+  real_t min_impurity_dec_;  // Minimal impurity decrease to split a node
+  real_t min_impurity_;  // Minimal impurity to split a node
 
   DISALLOW_COPY_AND_ASSIGN(DTree);
 };
